9d50c5866ca8efff0d287525feb1e179d084829f
nwdxlgzs <47502008+nwdxlgzs@users.noreply.github.com>
2020-02-27 10:17:20 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,32 @@
+appname="Carbon浏览器"
+packagename="github.nwdxlgzs.carbonbrowser"
+theme="Theme_DeviceDefault_Light_NoActionBar"
+developer="难忘的旋律"
+description="Carbon浏览器由github@nwdxlgzs制作"
+user_permission={
+  "ACCESS_ASSISTED_GPS",
+  "ACCESS_BACKGROUND_LOCATION",
+  "ACCESS_COARSE_LOCATION",
+  "ACCESS_FINE_LOCATION",
+  "ACCESS_GPS",
+  "ACCESS_LOCATION",
+  "ACCESS_MEDIA_LOCATION",
+  "ACCESS_NETWORK_STATE",
+  "ACCESS_WIFI_STATE",
+  "CAMERA",
+  "CHANGE_NETWORK_STATE",
+  "CHANGE_WIFI_STATE",
+  "CLEAR_APP_CACHE",
+  "GET_PACKAGE_SIZE",
+  "INSTALL_SHORTCUT",
+  "INTERNET",
+  "READ_EXTERNAL_STORAGE",
+  "READ_HISTORY_BOOKMARKS",
+  "READ_PHONE_STATE",
+  "RECORD_AUDIO",
+  "REQUEST_INSTALL_PACKAGES",
+  "WAKE_LOCK",
+  "WRITE_EXTERNAL_STORAGE",
+  "WRITE_HISTORY_BOOKMARKS"
+}
+
